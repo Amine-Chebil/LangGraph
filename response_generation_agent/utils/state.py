1b948074c7d7d35ea577b54821787080607047ac
Amine Chebil <amine.chebil@episousse.com.tn>
2025-05-14 16:03:46 +0100

    Refactor AgentState: add agents_responses field to track multiple responses

--- response_generation_agent/utils/state.py
+++ response_generation_agent/utils/state.py
@@ -5,6 +5,7 @@ from typing import TypedDict, Annotated, Sequence
 class AgentState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
     inquiry_summary: str
+    agents_responses: str
     draft_email: str
     tone: str
     length: str
